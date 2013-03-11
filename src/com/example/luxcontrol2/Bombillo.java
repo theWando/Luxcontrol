61c42b701dace54b6da103cc9ccae5a68114f217
JORGE GIL <dragongil1988@hotmail.com>
2013-03-11 08:12:34 -0430

    Modificaciones para comunicacion

---
+++ src/com/example/luxcontrol2/Bombillo.java
@@ -0,0 +1,26 @@
+package com.example.luxcontrol2;
+
+public class Bombillo {
+
+	private boolean estado;
+	
+	private String nombre;
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
